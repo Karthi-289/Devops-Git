302b7582fefc9b3aebd0f99760af1835986b881b
Karthi-289 <134209126+Karthi-289@users.noreply.github.com>
2023-06-14 09:41:37 +0530

    Update first_script.sh

--- script/first_script.sh
+++ script/first_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #just echoing some lines
 echo "this is the run time variable"
-read runtimevar
 echo "below is the value that has entered"
-echo "run time value $runtimevar"
+echo "run time value $2"
